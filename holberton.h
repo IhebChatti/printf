cd913e4db2707411c745d4dbbaab248108ac6f4b
ihebchatti <chatti.iheb93@gmail.com>
2020-03-14 20:47:54 +0100

    headerFile

--- holberton.h
+++ holberton.h
@@ -1,5 +1,24 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+
+/**
+ * struct conversion_spec - Conversion specifire
+ * @spec: specifier
+ * @f: conversion function
+ *
+ * Description: a Conversion wrapper
+ */
+typedef struct spec
+{
+	char *c;
+	void (*f)(char *);
+} spec_t;
+
 int _printf(const char *format, ...);
-int _write(char c);
-#endif
+void _putchar(char);
+void string_conv(va_list);
+void char_conv(va_list);
+void decimal_conv(va_list);
+
+#endif /* printf */
