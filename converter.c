ce97e4a0255b16d6fbbeb1ee326d0d6453fe88bf
ihebchatti <chatti.iheb93@gmail.com>
2020-03-14 20:42:18 +0100

    converter

--- converter.c
+++ converter.c
@@ -1,11 +1,63 @@
 #include <unistd.h>
 #include <stdarg.h>
+
 /**
-*_write - write char to stdout
-*@s: char to be printed
-*@Return: 1 
-*/
-int _write(char s)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	return (write(1, &s, 1));
+	return (write(1, &c, 1));
+}
+
+void string_conv(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	while (*s)
+		_putchar(*s++);
+}
+
+void char_conv(va_list args)
+{
+	_putchar(va_arg(args, int));
+}
+/**
+*print_number - print an integer
+*@n: int
+*/
+void print_number(int n)
+{
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		if (n > 0)
+		{
+			_putchar ((n % 10) + '0');
+		}
+		else
+		{
+			_putchar (-n % 10 + '0');
+		}
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	{
+		_putchar ((n % 10) + '0');
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+	{
+		_putchar ('-');
+		_putchar ((-n % 10) + '0');
+	}
+	else if (n == 0)
+		_putchar ('0');
+}
+void decimal_conv(va_list args)
+{
+	int n = va_arg(args, int);
+	print_number(n);
 }
