fdf9b50a2fb72ed8c5ff37e5f190cbf90c0bf1a3
ihebchatti <chatti.iheb93@gmail.com>
2020-03-14 20:40:11 +0100

    update

--- _printf.c
+++ _printf.c
@@ -1,32 +1,46 @@
 #include "holberton.h"
 #include <stdarg.h>
+
 /**
-*
-*
-*
-*
-*/
+ * _printf - printf
+ * @format: the format
+ *
+ * Return: int
+ */
 int _printf(const char *format, ...)
 {
-	int i;
-	char *s;
+	int i = 0;
 	va_list args;
+	
 
 	va_start(args, format);
-	for (i = 0; i < 10; i++)
+
+	while(format[i])
 	{
 		if (format[i] != '%')
 		{
-			_write(format[i]);
-
-		} 
+			_putchar(format[i]);
+		}
+		else
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					char_conv(args);
+					break;
+				case 's':
+					string_conv(args);
+					break;
+				case 'd':
+					decimal_conv(args);
+					break;
+			}
+		}
+		i++;
 	}
-	_write('\n');
-	va_end(args);
-	return (0);
-}
 
-int main(void)
-{
-	_printf("holberton");
+	va_end(args);
+
+	return (i);
 }
