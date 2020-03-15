d049715942542306fbf0a6b4823e5a7b4ce8e05e
ihebchatti <chatti.iheb93@gmail.com>
2020-03-15 14:44:55 +0100

    printfFix

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdarg.h>
-
 /**
  * _printf - printf
  * @format: the format
@@ -23,58 +22,39 @@ int _printf(const char *format, ...)
 		{"i", printdecimal},
 		{NULL, NULL}
 	};
-
 	va_start(args, format);
-
-	while((f = format[i]) != '\0')
+	while ((f = format[i]) != '\0')
 	{
 		if (f != '%' && !last_is_percent)
 		{
 			_write(f);
 			length++;
 		}
-
 		if (f == '%' && !last_is_percent)
 		{
 			last_is_percent++;
 			i++;
 			continue;
 		}
-
 		if (f == '%' && last_is_percent)
 		{
 			_write('%');
 			length++;
 			last_is_percent *= 0;
 		}
-
 		if (f != '%' && last_is_percent)
 		{
 			j = 0;
-
-#define S(V) (specs + j)->V
-
-			while (S(c))
+			while ((specs + j)->c)
 			{
-				if (*S(c) == f)
-					S(f)(args);
+				if (*(specs + j)->c == f)
+					(specs + j)->f(args);
 				last_is_percent *= 0;
 				j++;
 			}
-
-#undef S
-
 		}
 		i++;
 	}
-
 	va_end(args);
-
 	return (length);
 }
-
-
-
-
-
-
