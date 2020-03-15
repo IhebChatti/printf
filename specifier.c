d2094b616e1e4f845b53aac324d116b13ca3f6ff
ihebchatti <chatti.iheb93@gmail.com>
2020-03-15 14:28:45 +0100

    specifierBettyFix

--- specifier.c
+++ specifier.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "holberton.h"
-
 /**
  * _write - writes the character c to stdout
  * @c: The character to print
@@ -13,7 +12,13 @@ int _write(char c)
 {
 	return (write(1, &c, 1));
 }
-
+/**
+ * printstring - prints a string to stdout
+ * @args: The string to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void printstring(va_list args)
 {
 	char *s;
@@ -22,14 +27,27 @@ void printstring(va_list args)
 	while (*s)
 		_write(*s++);
 }
-
+/**
+ * printchar - prints a char to stdout
+ * @args: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void printchar(va_list args)
 {
 	_write(va_arg(args, int));
 }
-
+/**
+ * printdecimal - prints a decimal number to stdout
+ * @args: The number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void printdecimal(va_list args)
 {
 	int n = va_arg(args, int);
+
 	numbertobuf(n);
 }
