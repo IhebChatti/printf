60abc8af47997ed51cb403fafdfb10083168c8fe
ihebchatti <chatti.iheb93@gmail.com>
2020-03-15 14:29:54 +0100

    converterBettyFix

--- converter.c
+++ converter.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 #include "holberton.h"
-
 /**
-*print_number - print an integer
+*numbertobuf - print an integer
 *@n: int
 */
 void numbertobuf(int n)
@@ -12,22 +11,22 @@ void numbertobuf(int n)
 		numbertobuf(n / 10);
 		if (n > 0)
 		{
-			_write ((n % 10) + '0');
+			_write((n % 10) + '0');
 		}
 		else
 		{
-			_write (-n % 10 + '0');
+			_write(-n % 10 + '0');
 		}
 	}
 	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
 	{
-		_write ((n % 10) + '0');
+		_write((n % 10) + '0');
 	}
 	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
 	{
-		_write ('-');
-		_write ((-n % 10) + '0');
+		_write('-');
+		_write((-n % 10) + '0');
 	}
 	else if (n == 0)
-		_write ('0');
+		_write('0');
 }
