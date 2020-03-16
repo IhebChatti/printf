afd6071538e191167e7985be6b32db014582f869
ihebchatti <chatti.iheb93@gmail.com>
2020-03-16 15:36:00 +0100

    Fix Betty Style

--- get_function.c
+++ get_function.c
@@ -9,12 +9,19 @@
 		{"%", percent_format},		\
 		{NULL, NULL}			\
 	}
+/**
+*get_formater - wrapper to get the function needed
+*@res: pointer type char
+*@format: const char
+*@args: va_list
+*Return: length of str
+*/
 int get_formater(char *res, const char *format, va_list args)
 {
 	char *it;
-	SPECIFIERS;
 	int i, percent = 0;
 
+	SPECIFIERS;
 	if (format == NULL)
 		return (0);
 	it = res;
