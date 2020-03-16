c299db61a556f814efd03c285ef0d373eb7fc465
KoeusIss <sebri.issam@gmail.com>
2020-03-16 16:45:05 +0100

    Fix the null exception

--- _printf.c
+++ _printf.c
@@ -14,6 +14,10 @@ int _printf(const char *format, ...)
 	va_list args;
 	char *result;
 
+	if (!format)
+		return (-1);
+	if (format[0] == '%' && !format[1])
+		return (-1);
 	result = malloc(1024);
 	if (!result)
 		return (0);
