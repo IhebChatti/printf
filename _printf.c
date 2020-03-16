26f1886250a1504f10eeeed1a86fa02a766c6600
KoeusIss <sebri.issam@gmail.com>
2020-03-16 15:26:59 +0100

    Fix printf betty style

--- _printf.c
+++ _printf.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * _printf - prints the input on the shell buffer
+ * @format: requested format
+ *
+ * Return: a length of the string outputed
+ */
 int _printf(const char *format, ...)
 {
 	int value;
 	va_list args;
 	char *result;
+
 	result = malloc(1024);
 	if (!result)
 		return (0);
@@ -19,5 +26,5 @@ int _printf(const char *format, ...)
 		_write(*result);
 		result++;
 	}
-	return value;
+	return (value);
 }
