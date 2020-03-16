59612aaa507ccbe095805679e394e610d69a19dd
ihebchatti <chatti.iheb93@gmail.com>
2020-03-16 15:30:20 +0100

    Fix Betty Style

--- formater.c
+++ formater.c
@@ -1,18 +1,33 @@
 #include "holberton.h"
 #include <unistd.h>
-
+/**
+*_write - write to stdout
+*@c: char to be written
+*Return: 0 on SUCCESS
+*/
 int _write(char c)
 {
 	return write(1, &c, 1);
 }
-
+/**
+*percent_format - percent handler
+*@str: pointer to pointer type char
+*@args: argument
+*Return: 0 on SUCCESS
+*/
 int percent_format(char **str, va_list args)
 {
 	**str = '%';
 	(*str)++;
 	return (0);
 }
-
+/**
+*char_format - char handler
+*@str: char to be written
+*@args: argument
+*Return: 1 on SUCCESS
+*-1 on FAILURE
+*/
 int char_format(char **str, va_list args)
 {
 	char r = va_arg(args, int);
@@ -22,7 +37,13 @@ int char_format(char **str, va_list args)
 	(*str)++;
 	return (1);
 }
-
+/**
+*string_format - string handler
+*@str: string to be written
+*@args: arguments
+*Return: 0 on SUCCESS
+*-1 on ERROR
+*/
 int string_format(char **str, va_list args)
 {
 	char *s;
@@ -39,4 +60,3 @@ int string_format(char **str, va_list args)
 	}
 	return (0);
 }
-
